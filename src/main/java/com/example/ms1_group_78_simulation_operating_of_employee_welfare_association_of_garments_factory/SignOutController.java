f7fea4aeff561f3dd461c5e3f733ef462c93cbab
Redhwanarobe <hamidredhwanarobe@gmail.com>
2025-07-29 15:31:40 +0600

    update signout fxid and on action button

---
+++ src/main/java/com/example/ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory/SignOutController.java
@@ -0,0 +1,26 @@
+package com.example.ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+
+public class SignOutController
+{
+    @javafx.fxml.FXML
+    private TextArea signoutTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void go_to_log_inOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void noSignOutOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void yesSignOutOA(ActionEvent actionEvent) {
+    }
+}
