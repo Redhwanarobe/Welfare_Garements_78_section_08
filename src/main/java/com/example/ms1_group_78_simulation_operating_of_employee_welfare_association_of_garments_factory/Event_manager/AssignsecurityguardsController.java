e0b19e07978709184e4284c0904181dae3be09ef
tuhinXtg <natuhin02@gmail.com>
2025-07-30 03:52:51 +0600

    Created all fxml for Event manager

---
+++ src/main/java/com/example/ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory/Event_manager/AssignsecurityguardsController.java
@@ -0,0 +1,26 @@
+package com.example.ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory.Event_manager;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class AssignsecurityguardsController
+{
+    @javafx.fxml.FXML
+    private TextField eventtittleTF;
+    @javafx.fxml.FXML
+    private TextField nameofworjerTF;
+    @javafx.fxml.FXML
+    private TextField contactofworkerTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void addButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void assignallButtonOA(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,5 +4,7 @@ module com.example.ms1_group_78_simulation_operating_of_employee_welfare_associa
 
 
     opens com.example.ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory to javafx.fxml;
+    opens com.example.ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory.Event_manager to javafx.fxml, javafx.base;
+    opens com.example.ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory.Training_coordinator to javafx.fxml, javafx.base;
     exports com.example.ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory;
 }
