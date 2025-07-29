4c0555c17292f00b681737682d5c02f02f812102
tuhinXtg <natuhin02@gmail.com>
2025-07-29 12:07:32 +0600

    created login fxml and controller

---
+++ src/main/java/com/example/ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory/LoginController.java
@@ -0,0 +1,24 @@
+package com.example.ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class LoginController
+{
+    @javafx.fxml.FXML
+    private TextField loginUseridTF;
+    @javafx.fxml.FXML
+    private TextField loginPasswordTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void signupButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void loginButtonOA(ActionEvent actionEvent) {
+    }
+}
