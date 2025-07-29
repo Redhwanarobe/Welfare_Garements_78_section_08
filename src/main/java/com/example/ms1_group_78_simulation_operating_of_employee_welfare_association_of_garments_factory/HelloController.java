4e980507725f446ee5bac54b6f8a1ddb548bf2e2
Redhwanarobe <hamidredhwanarobe@gmail.com>
2025-07-29 06:22:06 +0600

    creat new project

---
+++ src/main/java/com/example/ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory to javafx.fxml;
+    exports com.example.ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory;
+}
