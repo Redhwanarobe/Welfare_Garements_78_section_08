78ce46853ed2733c6ed1c98b4707f3f5cf120758
fahimkhan24 <fbheker512@gmail.com>
2025-07-30 22:16:50 +0600

    Legal_Advisor

---
+++ src/main/java/com/example/ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory/Legal_Advisor/DevelopmentTraining.java
@@ -0,0 +1,29 @@
+package com.example.ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory.Legal_Advisor;
+
+public class DevelopmentTraining
+{
+    @javafx.fxml.FXML
+    private TableColumn titleCol;
+    @javafx.fxml.FXML
+    private TableColumn topicCol;
+    @javafx.fxml.FXML
+    private TableColumn objectiveCol;
+    @javafx.fxml.FXML
+    private TableView tableViewTextfield;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void returnOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SubmitAndSaveOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void createOA(ActionEvent actionEvent) {
+    }
+}
