69c359a9b5164e1f53a138e1bba5f41ba0086901
fahimkhan24 <fbheker512@gmail.com>
2025-07-31 01:25:21 +0600

    Legal_Advisor

---
+++ src/main/java/com/example/ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory/Legal_Advisor/AssistCompansation.java
@@ -0,0 +1,35 @@
+package com.example.ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory.Legal_Advisor;
+
+public class AssistCompansation
+{
+    @javafx.fxml.FXML
+    private TableColumn DescriptionCol;
+    @javafx.fxml.FXML
+    private TableColumn DateCol;
+    @javafx.fxml.FXML
+    private TableColumn reportIdCol;
+    @javafx.fxml.FXML
+    private TextArea showDetailsTextArea;
+    @javafx.fxml.FXML
+    private TableView TableViewfxid;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void checkReportOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void submitButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void verifyOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ShowOA(ActionEvent actionEvent) {
+    }
+}
