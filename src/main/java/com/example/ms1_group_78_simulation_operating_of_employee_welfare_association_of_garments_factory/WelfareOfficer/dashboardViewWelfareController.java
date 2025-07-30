c7455daf3abd96fdde286925898110e0f74ae75a
sadmansakibornob <sadmansakibornob2002@gmail.com>
2025-07-30 19:49:33 +0600

    Financial Officer Created

---
+++ src/main/java/com/example/ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory/WelfareOfficer/dashboardViewWelfareController.java
@@ -0,0 +1,7 @@
+package com.example.ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory.WelfareOfficer;
+
+public class dashboardViewWelfareController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,13 +4,12 @@ module com.example.ms1_group_78_simulation_operating_of_employee_welfare_associa
 
 
     opens com.example.ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory to javafx.fxml;
-<<<<<<< HEAD
     opens mainpkg.welfare_germents.Legal_Advior to javafx.fxml, javafx.base ;
     opens mainpkg.welfare_germents.Medical_Officer to javafx.fxml, javafx.base ;
 
-=======
     opens com.example.ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory.Event_manager to javafx.fxml, javafx.base;
     opens com.example.ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory.Training_coordinator to javafx.fxml, javafx.base;
->>>>>>> c08dabc4d2eba95aaf0275d47778ace8ff008e44
+    opens com.example.ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory.FinancialOfficer to javafx.fxml, javafx.base;
+    opens com.example.ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory.WelfareOfficer to javafx.fxml, javafx.base;
     exports com.example.ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory;
 }
