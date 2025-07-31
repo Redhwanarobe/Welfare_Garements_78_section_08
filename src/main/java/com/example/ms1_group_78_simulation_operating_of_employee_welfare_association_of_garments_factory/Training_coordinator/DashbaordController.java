9456579adb65854861cffec38c94dae91cde4d3b
tuhinXtg <natuhin02@gmail.com>
2025-07-31 20:29:02 +0600

    training_coordinator

---
+++ src/main/java/com/example/ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory/Training_coordinator/DashbaordController.java
@@ -0,0 +1,55 @@
+package com.example.ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory.Training_coordinator;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+public class DashbaordController
+{
+    @javafx.fxml.FXML
+    private VBox vbox;
+    @javafx.fxml.FXML
+    private AnchorPane anchorphane;
+    @javafx.fxml.FXML
+    private BorderPane borderphane;
+    @javafx.fxml.FXML
+    private TextArea dashboardTA;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void attendanceButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void budgetButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void upmaterialsButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void assignguestsperakerOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void createprogrammsButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void programmsinfoButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void givrfeedbackButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void assigntrainerButtomOA(ActionEvent actionEvent) {
+    }
+}
