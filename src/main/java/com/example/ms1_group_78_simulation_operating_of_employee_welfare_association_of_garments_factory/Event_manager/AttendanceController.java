a85d2ba1c97ae31ccf3ac3d91e004a920e979037
tuhinXtg <natuhin02@gmail.com>
2025-08-10 18:15:41 +0600

    eventlist

--- src/main/java/com/example/ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory/Event_manager/AttendanceController.java
+++ src/main/java/com/example/ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory/Event_manager/AttendanceController.java
@@ -1,6 +1,7 @@
 package com.example.ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory.Event_manager;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
 public class AttendanceController
@@ -9,9 +10,15 @@ public class AttendanceController
     private TextField attendees;
     @javafx.fxml.FXML
     private TextField totalinvitedTF;
+    @javafx.fxml.FXML
+    private ComboBox<String> choseeventCB;
 
     @javafx.fxml.FXML
     public void initialize() {
+        for (Events events : Event_details.getEventsObservableList()){
+            choseeventCB.getItems().add(events.getTitle());
+        }
+
     }
 
     @javafx.fxml.FXML
