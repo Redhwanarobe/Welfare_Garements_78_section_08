6388e9e251d590bbb0b0e49d1f2feb663d4051e9
Redhwanarobe <hamidredhwanarobe@gmail.com>
2025-08-12 23:29:10 +0600

    hello application

--- src/main/java/com/example/ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory/HelloApplication.java
+++ src/main/java/com/example/ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory/HelloApplication.java
@@ -10,13 +10,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-<<<<<<< HEAD
+
 
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
 
-=======
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Training_coordinator/Dashbaord.fxml"));
->>>>>>> 145c0257a6a76727c87b6fdb693490668dfc9767
+
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
