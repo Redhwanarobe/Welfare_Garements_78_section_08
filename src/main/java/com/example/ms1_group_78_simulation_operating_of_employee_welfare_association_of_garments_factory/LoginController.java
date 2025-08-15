67f23243eb10e0ad1c3ff49b1884f3579f519fcf
sadmansakibornob <sadmansakibornob2002@gmail.com>
2025-08-15 14:59:47 +0600

    Welfare/financial

--- src/main/java/com/example/ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory/LoginController.java
+++ src/main/java/com/example/ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory/LoginController.java
@@ -1,141 +1,24 @@
 package com.example.ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory;
 
-import com.example.ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory.Member.MemberDashboard;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.time.LocalDate;
-
-public class LoginController {
-    @FXML
+public class LoginController
+{
+    @javafx.fxml.FXML
     private TextField loginUseridTF;
-    @FXML
+    @javafx.fxml.FXML
     private TextField loginPasswordTF;
 
-
-    ObservableList<MemberDashboard> memberDashboardObservableList =FXCollections.observableArrayList();
-    @FXML
-    private AnchorPane anchorpane;
-
-
-    @FXML
+    @javafx.fxml.FXML
     public void initialize() {
-
-
     }
 
-    @FXML
+    @javafx.fxml.FXML
     public void signupButtonOA(ActionEvent actionEvent) {
-        // TODO: Implement sign-up logic
     }
 
-    @FXML
-    public void forgetpassworfButtonOA(ActionEvent actionEvent) {
-        // TODO: Implement forgot-password logic
+    @javafx.fxml.FXML
+    public void loginButtonOA(ActionEvent actionEvent) {
     }
-
-    @FXML
-    public void loginButtonOA(ActionEvent actionEvent) throws IOException {
-        String id = loginUseridTF.getText();
-        String password = loginPasswordTF.getText();
-        boolean flag = true;
-
-        Alert erroralert = new Alert(Alert.AlertType.ERROR);
-
-        if (id.isBlank()) {
-            flag = false;
-            erroralert.setTitle("User ID Error");
-            erroralert.setContentText("User ID cannot be blank");
-            erroralert.showAndWait();
-        }
-        if (password.isBlank()) {
-            flag = false;
-            erroralert.setTitle("Password Error");
-            erroralert.setContentText("Password cannot be blank");
-            erroralert.showAndWait();
-        }
-
-        if (flag) {
-            if (id.length() == 4) {
-//             login as event manager
-            } else if (id.length() == 5) {
-                // log in as fahim 1
-            } else if (id.length() == 3) {
-                // login fahim2
-            }
-            else if (id.length() == 6) {
-                FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Member/memberDashboard-view.fxml"));
-                Scene scene = new Scene(fxmlLoader.load());
-                Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-                stage.setScene(scene);
-                stage.setTitle("Member Dashboard");
-                stage.show();
-
-
-<<<<<<< HEAD
-            }
-=======
-        }
->>>>>>> fa9fbe5033ab958c168f66aec5e3e60910a056e6
-            else if (id.length() == 9){
-                FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("President_UnionLeader/presidentDashboard.fxml"));
-                Scene scene = new Scene(fxmlLoader.load());
-                Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-                stage.setScene(scene);
-                stage.setTitle("PresientDashbord");
-                stage.show();
-
-            }
-            else {
-
-                erroralert.setTitle("User ID Error");
-                erroralert.setContentText("User ID does not exist");
-                erroralert.showAndWait();
-            }
-        }
-
-    } }
-
-<<<<<<< HEAD
-//        if(id.length() == 3 ){
-=======
-    //        if(id.length() == 3 ){
->>>>>>> fa9fbe5033ab958c168f66aec5e3e60910a056e6
-//            // log in as a member
-//        }
-//        else if(id.length()== 4){
-//            // login as a president
-//        }
-//        else {
-//            erroralert.setTitle("user id error");
-//            erroralert.setTitle("user id do not exits");
-//            erroralert.showAndWait();
-//        }
-//        /*if (flag) {
-//            if (id.length() == 4) {
-//                //login as an event manager
-//
-//                for (Eventmanager eventmanager : eventmanagerObservableList){
-//                    if (eventmanager.login(id,password) != null){
-//                        Parent root = null;
-//                        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Event_manager/eventdashboard.fxml"));
-//                        root = fxmlLoader.load();
-//                        EventdashboardController eventdashboardController = fxmlLoader.getController();
-//                        eventdashboardController.setter(eventmanager);
-//                        Scene scene = new Scene(root);
-//                        Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
-//                        stage.setScene(scene);
-//                        stage.setTitle("Admin Dashboard");
-//                        stage.show();
-//                    }
-//                }
+}
--- src/main/java/com/example/ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory/User.java
+++ src/main/java/com/example/ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory/User.java
@@ -1,21 +1,41 @@
 package com.example.ms1_group_78_simulation_operating_of_employee_welfare_association_of_garments_factory;
 
-import java.time.LocalDate;
-import java.util.Objects;
 
-public abstract class  User {
-    private String id,name,phoneNo,email,address,password;
-    private LocalDate dob ,doj;
+import java.io.Serializable;
 
-    public User(String name, String phoneNo, String email, String address, String password, LocalDate dob) {
+public abstract class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private String id;
+    private String phone;
+    private String address;
+    private String password;
+    private String dob;
+    private String gender;
+    private String role;
 
+    public User() {
+    }
+    public User(String name, String id, String phone, String address, String password,
+                String dob, String gender, String role) {
         this.name = name;
-        this.phoneNo = phoneNo;
-        this.email = email;
+        this.id = id;
+        this.phone = phone;
         this.address = address;
         this.password = password;
         this.dob = dob;
-        this.doj= LocalDate.now();
+        this.gender = gender;
+        this.role = role;
+    }
+
+    // Getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getId() {
@@ -26,28 +46,12 @@ public abstract class  User {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getPhone() {
+        return phone;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhoneNo() {
-        return phoneNo;
-    }
-
-    public void setPhoneNo(String phoneNo) {
-        this.phoneNo = phoneNo;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 
     public String getAddress() {
@@ -66,25 +70,30 @@ public abstract class  User {
         this.password = password;
     }
 
-    public LocalDate getDob() {
+    public String getDob() {
         return dob;
     }
 
-    public void setDob(LocalDate dob) {
+    public void setDob(String dob) {
         this.dob = dob;
     }
 
-    @Override
-    public String toString() {
-        return
-                "Id= " + id + '\n' +
-                "Name= " + name + '\n' +
-                "PhoneNo= " + phoneNo + '\n' +
-                "Email= " + email + '\n' +
-                "Address= " + address + '\n' +
-                "Password= " + password + '\n' +
-                "Date of birth= " + dob ;
+    public String getGender() {
+        return gender;
     }
-    public abstract String generateID();
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    // Abstract method to be implemented by subclasses
     public abstract User login(String id, String password);
 }
